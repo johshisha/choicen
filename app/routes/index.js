8ddf16547af1885451db8e5b822f819f22e60568
johshisha <johshisha@gmail.com>
2018-03-04 13:28:35 -0700

    Show question in index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var models = require("../models");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var user_id = req.query.user_id || 1;
+  var question_id = 1;
+  models.Question.find(
+    {
+      "where": {"id": question_id},
+    }
+  ).then(function(question){
+    console.log("question:" + question);
+    question.getOptions().then(associatedOptions => {
+      // associatedTasks is an array of tasks
+      var options = associatedOptions;
+      console.log("question.options:" + options);
+      // res.send({"title": question.title, "options": options});
+      res.render("index", { question: question.title, "options": options });
+    });
+  }).catch(function(reason) {
+    console.log(reason);
+  });
 });
 
 module.exports = router;
