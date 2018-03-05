6b07576ddcf6aa138eaf86bdcb0ca979e7f2bc20
johshisha <johshisha@gmail.com>
2018-03-05 01:33:41 -0700

    [WIP] Show questions list 非同期の処理を待てなくて困っている

--- app/config/insert_dummy_data.sql
+++ app/config/insert_dummy_data.sql
@@ -1,9 +1,19 @@
 insert into Users (name, createdAt, UpdatedAt) values ("test", now(), now());
 insert into Users (name, createdAt, UpdatedAt) values ("test2", now(), now());
 
+-- Question 1
 insert into Questions (title, createdAt, UpdatedAt) values ("好きなエディタは？", now(), now());
 
 insert into Options (name, questionId, createdAt, UpdatedAt) values ("Vim", 1, now(), now());
 insert into Options (name, questionId, createdAt, UpdatedAt) values ("Emacs", 1, now(), now());
 
 insert into Votes (userId, optionId, questionId, createdAt, UpdatedAt) values (2, 1, 1, now(), now());
+
+-- Question 2
+insert into Questions (title, createdAt, UpdatedAt) values ("アメリカ横断", now(), now());
+
+insert into Options (name, questionId, createdAt, UpdatedAt) values ("する", 2, now(), now());
+insert into Options (name, questionId, createdAt, UpdatedAt) values ("したい", 2, now(), now());
+insert into Options (name, questionId, createdAt, UpdatedAt) values ("しない", 2, now(), now());
+
+insert into Votes (userId, optionId, questionId, createdAt, UpdatedAt) values (2, 3, 2, now(), now());
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,22 +5,28 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var user_id = req.query.user_id || 1;
-  var question_id = 1;
-  models.Question.find(
-    {
-      "where": {"id": question_id},
-    }
-  ).then(function(question){
-    console.log("question:" + question);
-    question.getOptions().then(associatedOptions => {
-      // associatedTasks is an array of tasks
-      var options = associatedOptions;
-      console.log("question.options:" + options);
-      // res.send({"title": question.title, "options": options});
-      res.render("index", { question: question.title, "options": options });
+  models.User.findById(user_id).then(user => {
+    var question_id = 1;
+    var ret = []
+    models.Question.all().then(questions => {
+      for (var question of questions){
+        models.Vote.findOne({ where: {userId: user.id, questionId: question.id} }).then(vote => {
+          console.log("vote: " + vote);
+          question.getOptions().then(associatedOptions => {
+            // associatedTasks is an array of tasks
+            var options = associatedOptions;
+            ret.push({ question: question.title, options: options, voted_id: vote.optionId || undefined })
+            console.log("RET: " + ret);
+          });
+        });
+      }
+      // res.render("index", { ret: ret });
+    }).then(() => {
+      console.log("ret: " + ret);
+      res.send({"ret": ret});
+    }).catch(function(reason) {
+      console.log(reason);
     });
-  }).catch(function(reason) {
-    console.log(reason);
   });
 });
 
