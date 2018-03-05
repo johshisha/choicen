fdec036ac0a2bac6d089291536a6ccd8dbcfb862
johshisha <johshisha@gmail.com>
2018-03-05 11:57:27 -0700

    Show questions list Arrayの長さを見て，setIntervalで待機する方法にした． 後でPromiseとかに変更したい

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,24 +6,30 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var user_id = req.query.user_id || 1;
   models.User.findById(user_id).then(user => {
-    var question_id = 1;
     var ret = []
     models.Question.all().then(questions => {
-      for (var question of questions){
+      var all_length = questions.length;
+      questions.forEach(question => {
         models.Vote.findOne({ where: {userId: user.id, questionId: question.id} }).then(vote => {
           console.log("vote: " + vote);
+          var voted_id = vote ? vote.optionId : undefined
           question.getOptions().then(associatedOptions => {
             // associatedTasks is an array of tasks
             var options = associatedOptions;
-            ret.push({ question: question.title, options: options, voted_id: vote.optionId || undefined })
+            ret.push({ question: question.title, options: options, voted_id: voted_id })
             console.log("RET: " + ret);
           });
         });
-      }
-      // res.render("index", { ret: ret });
-    }).then(() => {
-      console.log("ret: " + ret);
-      res.send({"ret": ret});
+      });
+      console.log(ret.length)
+      var timer = setInterval(() => {
+        console.log(ret.length + ", " + all_length)
+        if (ret.length == all_length) {
+          clearInterval(timer);
+          // res.send({"ret": ret});
+          res.render("index", { ret: ret });
+        }
+      }, 100)
     }).catch(function(reason) {
       console.log(reason);
     });
