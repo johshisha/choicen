06c511537fbfcb67667cedb2eb8f10135e86261d
johshisha <johshisha@gmail.com>
2018-03-07 14:33:06 -0700

    Add form for create a question And move question list page from `/` to `questions/`

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,36 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var user_id = req.query.user_id || 1;
-  models.User.findById(user_id).then(user => {
-    var ret = []
-    models.Question.all().then(questions => {
-      var all_length = questions.length;
-      questions.forEach(question => {
-        models.Vote.findOne({ where: {userId: user.id, questionId: question.id} }).then(vote => {
-          console.log("vote: " + vote);
-          var voted_id = vote ? vote.optionId : undefined
-          question.getOptions().then(associatedOptions => {
-            // associatedTasks is an array of tasks
-            var options = associatedOptions;
-            ret.push({ question: question.title, options: options, voted_id: voted_id })
-            console.log("RET: " + ret);
-          });
-        });
-      });
-      console.log(ret.length)
-      var timer = setInterval(() => {
-        console.log(ret.length + ", " + all_length)
-        if (ret.length == all_length) {
-          clearInterval(timer);
-          // res.send({"ret": ret});
-          res.render("index", { ret: ret });
-        }
-      }, 100)
-    }).catch(function(reason) {
-      console.log(reason);
-    });
-  });
+  var text = "Welcome to Choicen!"
+  res.render("index", { text: text });
 });
 
 module.exports = router;
---
+++ app/routes/questions.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var models = require("../models");
+var router = express.Router();
+
+// index
+router.get('/', function(req, res, next) {
+  var user_id = req.query.user_id || 1;
+  models.User.findById(user_id).then(user => {
+    var ret = []
+    models.Question.all().then(questions => {
+      var all_length = questions.length;
+      questions.forEach(question => {
+        models.Vote.findOne({ where: {userId: user.id, questionId: question.id} }).then(vote => {
+          var voted_id = vote ? vote.optionId : undefined
+          question.getOptions().then(associatedOptions => {
+            // associatedTasks is an array of tasks
+            var options = associatedOptions;
+            ret.push({ question: question.title, options: options, voted_id: voted_id })
+          });
+        });
+      });
+      console.log(ret.length)
+      var counter = setInterval(() => {
+        if (ret.length == all_length) {
+          clearInterval(counter);
+          // res.send({"ret": ret});
+          res.render("questions/index", { ret: ret });
+        }
+      }, 100)
+    }).catch(function(reason) {
+      console.log(reason);
+    });
+  });
+});
+
+// create
+router.post('/create', function(req, res) {
+  models.Question.create({
+    title: req.body.title
+  }).then(question => {
+    var question_id = question.id;
+    req.body.options.forEach(option_name => {
+      models.Option.create({
+        name: option_name,
+        questionId: question_id
+      });
+    })
+    res.redirect('/questions');
+  });
+});
+
+module.exports = router;
